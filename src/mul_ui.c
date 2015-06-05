72ec3cbeedc03500e53aa47d86c264d9015fc641
vlefevre <vlefevre@280ebfd0-de03-0410-8827-d642c229c3f4>
2015-06-05 08:34:34 +0000

    [src/mul_ui.c] Fixed bug in mpfr_mul_ui/si: the inexact flag wasn't set. (merged changeset r9505 from the trunk)
    git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/branches/3.1@9516 280ebfd0-de03-0410-8827-d642c229c3f4

--- src/mul_ui.c
+++ src/mul_ui.c
@@ -115,7 +115,7 @@ mpfr_mul_ui (mpfr_ptr y, mpfr_srcptr x, unsigned long int u, mpfr_rnd_t rnd_mode
   MPFR_SET_EXP (y, MPFR_GET_EXP (x) + cnt);
   MPFR_SET_SAME_SIGN (y, x);
 
-  return inexact;
+  MPFR_RET (inexact);
 }
 
 int mpfr_mul_si (mpfr_ptr y, mpfr_srcptr x, long int u, mpfr_rnd_t rnd_mode)
