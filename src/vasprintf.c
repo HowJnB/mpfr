HowJnB/mpfr

commit e64dbd3bb941f871dd2be9036bf37a6c677da773

[src/vasprintf.c] avoid memory allocation for snprintf with size=0
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@11273 280ebfd0-de03-0410-8827-d642c229c3f4

src/vasprintf.c
@@ -1755,6 +1755,12 @@ sprnt_fp (struct string_buffer *buf, mpfr_srcptr p,
   if (length < 0)
     return -1;
 
+  if (spec.size == 0) /* no need to fill the buffer */
+    {
+      buf->curr = buf->start + length;
+      goto clear_and_exit;
+    }
+
   /* right justification padding with left spaces */
   if (np.pad_type == LEFT && np.pad_size != 0)
     buffer_pad (buf, ' ', np.pad_size);
@@ -1809,6 +1815,7 @@ sprnt_fp (struct string_buffer *buf, mpfr_srcptr p,
   if (np.pad_type == RIGHT && np.pad_size != 0)
     buffer_pad (buf, ' ', np.pad_size);
 
+ clear_and_exit:
   clear_string_list (np.sl);
   return length;
 }
@@ -2104,12 +2111,7 @@ mpfr_vasnprintf_aux (char **ptr, char *Buf, size_t size, const char *fmt,
 
   va_end (ap2);
   nbchar = buf.curr - buf.start;
-  MPFR_ASSERTD (nbchar == strlen (buf.start));
-  buf.start =
-    (char *) (*__gmp_reallocate_func) (buf.start, buf.size, nbchar + 1);
-  buf.size = nbchar + 1; /* update needed for __gmp_free_func below when
-                            nbchar is too large (overflow_error) */
-  
+
   if (ptr == NULL) /* implement mpfr_vsnprintf */
     {
       if (size > 0)
@@ -2132,6 +2134,12 @@ mpfr_vasnprintf_aux (char **ptr, char *Buf, size_t size, const char *fmt,
                         the terminating null character */
     }
 
+  MPFR_ASSERTD (nbchar == strlen (buf.start));
+  buf.start =
+    (char *) (*__gmp_reallocate_func) (buf.start, buf.size, nbchar + 1);
+  buf.size = nbchar + 1; /* update needed for __gmp_free_func below when
+                            nbchar is too large (overflow_error) */
+  
   /* below we implement mpfr_vasprintf */
   *ptr = buf.start;
 

tests/tsprintf.c
@@ -88,6 +88,11 @@ check_sprintf (const char *expected, const char *fmt, mpfr_srcptr x)
       printf ("Error in mpfr_snprintf (s, %d, \"%s\", x) return value\n",
               p, fmt);
       printf ("expected: %d\ngot:      %d\n", n0, n1);
+      char format[1024];
+      strncpy (format, "x='", 1024);
+      strncpy (format + 3, fmt, 1021);
+      strncpy (format + 3 + strlen (fmt), "'\n", 1021 - strlen (fmt));
+      mpfr_printf (format, x);
       exit (1);
     }
   if ((p > 1 && strncmp (expected, buffer, p-1) != 0)

commit 0afaeddb2625c1278dc813a97cb06153524b7422

[src/vasprintf.c] Fixed bug introduced in r11273.
git-svn-id: https://scm.gforge.inria.fr/anonscm/svn/mpfr/trunk@11278 280ebfd0-de03-0410-8827-d642c229c3f4

src/vasprintf.c
@@ -1757,7 +1757,7 @@ sprnt_fp (struct string_buffer *buf, mpfr_srcptr p,
 
   if (spec.size == 0) /* no need to fill the buffer */
     {
-      buf->curr = buf->start + length;
+      buf->curr += length;
       goto clear_and_exit;
     }
 